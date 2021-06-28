8b4c4a3bb190b02adb9a3ce02cff5ff65e7b2f93
CC0620 <chenabernard@gmail.com>
2021-06-28 23:33:27 +0900

    ハッシュ

--- hash.rb
+++ hash.rb
@@ -1,14 +1,32 @@
-data = {
-    "name":"大泉",
-    "gender":"男性",
-    "age":46
-}
-data[:address] = "東京"
+# data = {
+#     "name":"大泉",
+#     "gender":"男性",
+#     "age":46
+# }
+# data[:children] =["太郎","次郎","三郎"]
 
-puts data[:name]
-puts data [:gender]
-puts data[:age]
-puts data[:address]
+# puts data
+
+# puts data[:children]
+
+dic = {
+    "x":"10",
+    "y":"20",
+}
+dic[:y]="40"
+
+puts dic
+
+# data[:gender] ="女性"
+
+# puts data
+# puts data[:gender]
+# data[:address] = "東京"
+
+# puts data[:name]
+# puts data [:gender]
+# puts data[:age]
+# puts data[:address]
 
 # data = ["大泉","男性",46]
 
