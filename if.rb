6a568f0a96277373de019a4b40a13edaa4f824e3
CC0620 <chenabernard@gmail.com>
2021-06-30 01:14:56 +0900

    if文

---
+++ if.rb
@@ -0,0 +1,19 @@
+# num = 10
+# if num >= 10 then
+#     puts "Hello World!"
+# end
+
+# num = 10
+# if num < 10 then
+#     puts "Hello World!"
+# end
+
+num = 11
+
+if num > 12 then 
+    puts "numは12より大きい"
+elsif num < 23 and num >12 then
+    puts "numは23より小さく、かつ12より大きい"
+else 
+    puts "numはその他の数字です"
+end
