1d4a38ed3f83be194ad25c8efd9e685d72b78bf8
CC0620 <chenabernard@gmail.com>
2021-07-06 17:47:57 +0900

    if文を使った条件分岐

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,3 +2,34 @@
 # ３の倍数の時はFizz
 # 5の倍数の時はBuzz
 # 3と5の倍数の時はFizzBuzz
+
+# for i in 1..100 do
+# if i % 15 == 0
+#     puts "FizzBuzz"
+#      elsif i % 3 == 0
+#         puts "Fizz"
+#     elsif i % 5 == 0
+#         puts"Buzz"
+    
+#     else
+#         puts i
+#     end
+# end
+
+num = 1
+while num <= 100 do
+  if num % 15 == 0
+    puts "fizzbuzz"
+  elsif num % 5 == 0
+         puts "buzz"
+  elsif num % 3 == 0
+    puts "fizz"
+  else
+    puts num
+  end
+  num += 1
+end
+
+
+
+
--- if.rb
+++ if.rb
@@ -8,12 +8,19 @@
 #     puts "Hello World!"
 # end
 
-num = 11
+# num = 11
 
-if num > 12 then 
-    puts "numは12より大きい"
-elsif num < 23 and num >12 then
-    puts "numは23より小さく、かつ12より大きい"
-else 
-    puts "numはその他の数字です"
-end
+# if num > 12 then 
+#     puts "numは12より大きい"
+# elsif num < 23 and num >12 then
+#     puts "numは23より小さく、かつ12より大きい"
+# else 
+#     puts "numはその他の数字です"
+# end
+
+# def check(num)
+# if num == 42 then
+# puts "Answer to the Ultimate Question of Life, the Universe, and Everything"
+# end
+# end
+# check(42)
