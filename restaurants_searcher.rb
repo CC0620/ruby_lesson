24b1a81c9ad719a7e8b1164492a621d2e8db211f
CC0620 <chenabernard@gmail.com>
2021-07-11 23:00:23 +0900

    APIからデータを取得するアプリを仕上げよう　

--- restaurants_searcher.rb
+++ restaurants_searcher.rb
@@ -1,11 +1,17 @@
 def write_data_to_csv()
     restaurants = []
-    response = {hogehoge:"hogehoge"}
+    response = "hogehoge"
+
     if response.has_key?("erroe")then
         puts"エラーが発生しました!"
     end
+    for restaurant in response["results"]["shop"]do
+        rest_name = restaurant["name"]
+        restaurants.append(rest_name)
+    end
 
+    File.open("restaurants_list.csv", "w") do |file|
+    end
 
     return print restaurants
 end
-    write_data_to_csv
